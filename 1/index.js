5009530c5066a6a56bac2708484905c1566f9050
Laura Read <laurajmtb@gmail.com>
2024-08-11 16:13:40 +0000

    adding webdev'd dropdowm tutorial just started

--- 1/index.js
+++
@@ -1,8 +0,0 @@
-const body = document.getElementsByTagName("body")[0]
-body.style.backgroundColor = "red"
-
-function setColor(name) {
-    body.style.backgroundColor = name;
-}
-
-setColor("green")
